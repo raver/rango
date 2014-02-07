ff0da4e254cbffae8d2952e4bcef96afe788be02
Jude <yutao008@gmail.com>
2014-02-07 19:50:53 +0800

    after chapter 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from rango import views
 
+# At the top of your urls.py file, add the following line:
+from django.conf import settings
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -17,3 +20,12 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls'))
 )
+
+
+# UNDERNEATH your urlpatterns definition, add the following two lines:
+if settings.DEBUG:
+        urlpatterns += patterns(
+                    'django.views.static',
+                    (r'media/(?P<path>.*)',
+                             'serve',
+                             {'document_root': settings.MEDIA_ROOT}), )
